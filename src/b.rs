6738973434e9c9f1655dae817cdad80d2dcff392
klee <k.r.okayama@gmail.com>
2021-09-23 22:48:51 +0900

    add use

--- src/b.rs
+++ src/b.rs
@@ -1,5 +1,9 @@
-use proconio::input;
+use cmp::{max, min, Reverse};
+use proconio::{fastout, input, marker::*};
+use std::collections::*;
+use std::*;
 
+const INF: usize = 1000000;
 fn main() {
     input! {
         n: usize,
